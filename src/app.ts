7e428882bc1b7a68ed19592acb61657a66ee5e68
Talerchik-Aleksey <atalerchik@twelvedevs.com>
2023-06-01 22:21:04 +0300

    feat: add compression and logger for requests

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,18 @@ import userRoutes from "./routes";
 import helmet from "helmet";
 import { errorBoundaryMiddleware } from "./middlewares/middlewares";
 import { connect } from "./libs/sequelize";
+import PinoHttp from "pino-http";
+import { logger } from "./libs/logger";
+import compression from "compression";
 
 const app: Application = express();
-
 Promise.all([connect()]);
+app.use(compression());
+app.use(
+  PinoHttp({
+    logger,
+  }),
+);
 app.use(helmet());
 app.use(json());
 app.use(errorBoundaryMiddleware);
