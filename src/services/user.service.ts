ff1db80971b2bb3e36d8a29416acfb3427e73432
Talerchik-Aleksey <atalerchik@twelvedevs.com>
2023-06-03 11:49:15 +0300

    feat: add activation token for create endpoint

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,12 +1,14 @@
 import { User } from "../models/User";
 import { UpdateUserBody } from "../types/user";
+import { generateToken } from "../utils/generateToken";
 
 export async function getUser(searchParams: Record<string, any>) {
   return await User.findOne(searchParams);
 }
 
 export async function createUser(name: string, email: string, password: string) {
-  return await User.create({ name, email, password });
+  const activationToken = generateToken();
+  return await User.create({ name, email, password, activationToken });
 }
 
 export async function updateUser(id: string, updateBody: UpdateUserBody) {
