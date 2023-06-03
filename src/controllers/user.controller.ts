3f9761c4d032ce79f1b6d305d306db4994990a55
Talerchik-Aleksey <atalerchik@twelvedevs.com>
2023-06-03 11:30:06 +0300

    add delete and update user endpoints

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { createUser, getUser } from "../services/user.service";
+import { createUser, deleteUser, getUser, updateUser } from "../services/user.service";
 import { HttpError } from "../utils/HttpError";
 import { errorResponse } from "../utils/errorResponse";
 import { successResponse } from "../utils/successRespons";
@@ -14,7 +14,7 @@ export async function createUserController(req: Request, res: Response) {
     }
 
     const newUser = await createUser(name, email, password);
-    successResponse(req, res, newUser);
+    successResponse(req, res, { user: newUser });
   } catch (error) {
     errorResponse(req, res, error);
   }
@@ -30,18 +30,41 @@ export async function getUserController(req: Request, res: Response) {
       throw new HttpError(404, "User not found");
     }
 
-    successResponse(req, res, user);
+    successResponse(req, res, { user });
   } catch (error) {
     errorResponse(req, res, error);
   }
 }
 
 export async function updateUserController(req: Request, res: Response) {
-  return res.status(200).json();
+  const { id } = req.params;
+
+  try {
+    const user = await getUser({ id });
+
+    if (!user) {
+      throw new HttpError(404, "User not found");
+    }
+
+    const updatedUser = await updateUser(id, req.body);
+
+    successResponse(req, res, { user: updatedUser });
+  } catch (error) {
+    errorResponse(req, res, error);
+  }
 }
 
 export async function deleteUserController(req: Request, res: Response) {
-  return res.status(200).json();
+  try {
+    const user = await getUser({ id: req.params.id });
+    if (!user) {
+      throw new HttpError(404, "User not found");
+    }
+    const deletedUser = await deleteUser(req.params.id);
+    successResponse(req, res, { user: deletedUser });
+  } catch (error) {
+    errorResponse(req, res, error);
+  }
 }
 
 export async function loginUserController(req: Request, res: Response) {
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,9 +1,29 @@
 import { User } from "../models/User";
+import { UpdateUserBody } from "../types/user";
 
 export async function getUser(searchParams: Record<string, any>) {
-  return User.findOne(searchParams);
+  return await User.findOne(searchParams);
 }
 
 export async function createUser(name: string, email: string, password: string) {
-  return User.create({ name, email, password });
+  return await User.create({ name, email, password });
+}
+
+export async function updateUser(id: string, updateBody: UpdateUserBody) {
+  return await User.update(
+    { ...updateBody },
+    {
+      where: {
+        id,
+      },
+    },
+  );
+}
+
+export async function deleteUser(id: string) {
+  return await User.destroy({
+    where: {
+      id,
+    },
+  });
 }
